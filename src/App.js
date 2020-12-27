f10b938bbb46388801882febcb2ef762b854f843
zakirangwala <zakirangwala@gmail.com>
2020-12-27 15:48:22 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import { Counter } from './features/counter/Counter';
+// import { Counter } from './features/counter/Counter';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
         <h1>LinkedIn Clone</h1>
-      </header>
     </div>
   );
 }
