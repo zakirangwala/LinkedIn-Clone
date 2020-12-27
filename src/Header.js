691b56f5391c11deccbf0ce41a10d6b795af3cde
zakirangwala <zakirangwala@gmail.com>
2020-12-27 16:52:51 -0500

    Header Options

--- src/Header.js
+++ src/Header.js
@@ -1,18 +1,24 @@
 import React from "react";
-import SearchIcon from '@material-ui/icons/Search';
+import SearchIcon from "@material-ui/icons/Search";
+import HeaderOption from "./HeaderOption.js";
 import "./Header.css";
 
 function Header() {
   return (
     <div>
       <div className="header__left">
-        <img src="https://image.flaticon.com/icons/png/512/61/61109.png" alt="Linkedin Logo" />
+        <img
+          src="https://cdn.worldvectorlogo.com/logos/linkedin-icon-2.svg"
+          alt="Linkedin Logo"
+        />
         <div className="header__search">
-            <SearchIcon />
-            <input type="text"></input>
+          <SearchIcon />
+          <input type="text"></input>
         </div>
       </div>
-      <div className="header__right"></div>
+      <div className="header__right">
+        <HeaderOption />
+      </div>
     </div>
   );
 }
