dbb7818f83e6b0965d3dd000639106ccb01b763c
zakirangwala <zakirangwala@gmail.com>
2020-12-28 13:02:12 -0500

    post input field + icons

--- src/Feed.js
+++ src/Feed.js
@@ -1,6 +1,10 @@
 import React from "react";
 import CreateIcon from "@material-ui/icons/Create";
 import InputOption from "./InputOption.js";
+import ImageIcon from "@material-ui/icons/Image";
+import SubscriptionsIcon from "@material-ui/icons/Subscriptions";
+import EventNoteIcon from "@material-ui/icons/EventNote";
+import CalendarViewDayIcon from "@material-ui/icons/CalendarViewDay";
 import "./Feed.css";
 
 function Feed() {
@@ -15,7 +19,14 @@ function Feed() {
           </form>
         </div>
         <div className="feed__inputOptions">
-          <InputOption />
+          <InputOption Icon={ImageIcon} title="Photo" color="#70B5F9" />
+          <InputOption Icon={SubscriptionsIcon} title="Video" color="#E7A33E" />
+          <InputOption Icon={EventNoteIcon} title="Event" color="#C0CBCD" />
+          <InputOption
+            Icon={CalendarViewDayIcon}
+            title="Write Article"
+            color="#7FC15E"
+          />
         </div>
       </div>
     </div>
--- src/InputOption.js
+++ src/InputOption.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./InputOption.css";
 
-function InputOption() {
-  return <div className="InputOption">
-      
-  </div>;
+function InputOption({ Icon, title, color }) {
+  return (
+    <div className="InputOption">
+      <Icon style={{ color: color }} />
+      <h4>{title}</h4>
+    </div>
+  );
 }
 
 export default InputOption;
