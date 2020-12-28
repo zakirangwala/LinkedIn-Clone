660d69cff79ad8577c8b29f05bf61f6df0518725
zakirangwala <zakirangwala@gmail.com>
2020-12-28 16:13:08 -0500

    make user instance

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import userReducer from '../features/userSlice.js';
 
 export default configureStore({
   reducer: {
-    counter: counterReducer,
+    counter: userReducer,
   },
 });
