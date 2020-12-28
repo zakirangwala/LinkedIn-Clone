f153f1005a3a7b4f80d816995ac28cca17055eda
zakirangwala <zakirangwala@gmail.com>
2020-12-28 12:32:04 -0500

    sidebar items

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,10 +3,20 @@ import "./Sidebar.css";
 import { Avatar } from "@material-ui/core";
 
 function Sidebar() {
+  const recentItem = (topic) => (
+    <div className="sidebar__recentItem">
+      <span className="sidebar__hash">#</span>
+      <p>{topic}</p>
+    </div>
+  );
+
   return (
     <div className="sidebar">
       <div className="sidebar__top">
-        <img src="https://wallpaperaccess.com/full/746344.jpg" alt="background-container-image" />
+        <img
+          src="https://wallpaperaccess.com/full/746344.jpg"
+          alt="background-banner"
+        />
         <Avatar className="sidebar__avatar" />
         <h2>Zaki Rangwala</h2>
         <h4>zakirangwala@gmail.com</h4>
@@ -23,6 +33,10 @@ function Sidebar() {
       </div>
       <div className="sidebar__bottom">
         <p>Recent</p>
+        {recentItem('reactjs')}
+        {recentItem('programming')}
+        {recentItem('software development')}
+        {recentItem('graphic design')}
       </div>
     </div>
   );
