7392731344ddf10998d1f31869a15154eb207a70
zakirangwala <zakirangwala@gmail.com>
2020-12-28 12:48:00 -0500

    input field

--- src/Feed.js
+++ src/Feed.js
@@ -1,12 +1,21 @@
-import React from 'react'
-import "./Feed.css"
+import React from "react";
+import CreateIcon from "@material-ui/icons/Create";
+import "./Feed.css";
 
 function Feed() {
-    return (
-        <div>
-            
+  return (
+    <div className="feed">
+      <div className="feed__inputContainer">
+        <div className="feed__input">
+          <CreateIcon />
+          <form>
+            <input type="text" />
+            <button type="submit">Send</button>
+          </form>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Feed
+export default Feed;
