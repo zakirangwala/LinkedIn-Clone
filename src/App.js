889da3b1b7e1b9b194140d12a9dd8ad0c008e98f
zakirangwala <zakirangwala@gmail.com>
2020-12-28 16:23:03 -0500

    redux user state

--- src/App.js
+++ src/App.js
@@ -2,16 +2,24 @@ import React from "react";
 import Header from "./Header.js";
 import Sidebar from "./Sidebar.js";
 import Feed from "./Feed.js";
+import Login from "./Login.js";
+import { useSelector } from "react-redux";
+import { selectUser } from "./features/userSlice";
 import "./App.css";
 
 function App() {
+  const user = useSelector(selectUser);
   return (
     <div className="app">
       <Header />
-      <div className="app__body">
-        <Sidebar />
-        <Feed />
-      </div>
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app__body">
+          <Sidebar />
+          <Feed />
+        </div>
+      )}
     </div>
   );
 }
