a2c202a937ffb16e7551f5969c246e1ad837034e
zakirangwala <zakirangwala@gmail.com>
2020-12-29 18:48:56 -0500

    avatar

--- src/HeaderOption.js
+++ src/HeaderOption.js
@@ -11,9 +11,7 @@ function HeaderOption({ avatar, Icon, title, onClick }) {
     <div onClick={onClick} className="headerOption">
       {Icon && <Icon className="headerOption__icon" />}
       {avatar && (
-        <Avatar className="headerOption__icon" src={user?.photoUrl}>
-          {user?.email[0]}
-        </Avatar>
+        <Avatar className="headerOption__icon" src={user?.photoUrl}></Avatar>
       )}
       <h3 className="headerOption__title">{title}</h3>
     </div>
