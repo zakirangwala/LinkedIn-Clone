ebce52426d3dbf92682853e28fcc56aaaa79889b
zakirangwala <zakirangwala@gmail.com>
2020-12-29 18:38:03 -0500

    add widgets

--- src/Widgets.js
+++ src/Widgets.js
@@ -1,12 +1,33 @@
-import React from 'react'
-import "./Widgets.css"
+import React from "react";
+import InfoIcon from "@material-ui/icons/Info";
+import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
+import "./Widgets.css";
 
 function Widgets() {
-    return (
-        <div className="widgets">
-            
-        </div>
-    )
+  const newsArticle = (heading, subtitle) => (
+    <div className="widgets__article">
+      <div className="widgets__articleLeft">
+        <FiberManualRecordIcon />
+      </div>
+      <div className="widgets__articleRight">
+        <h4>{heading}</h4>
+        <p>{subtitle}</p>
+      </div>
+    </div>
+  );
+
+  return (
+    <div className="widgets">
+      <div className="widgets__header">
+        <h2>LinkedIn News</h2>
+        <InfoIcon />
+      </div>
+      {newsArticle("Coronavirus Cases","The death toll is on the rise - 10K readers")}
+      {newsArticle("The Empire Strikes Back","Star Wars movie cancelled by the people - 4K readers")}
+      {newsArticle("Merry Christmas","Everyone worldwide experiences their first lockdown christmas - 2K readers")}
+
+    </div>
+  );
 }
 
-export default Widgets
+export default Widgets;
